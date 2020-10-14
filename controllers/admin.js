856b72709c4d2e731ff5efe93639840545961d47
Jitendera Pratap Singh <JITENDERA@users.noreply.github.com>
2020-10-13 20:23:59 -0500

    End of Section 9

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,7 +1,7 @@
 const Product = require('../models/product');
 
 exports.getAddedProduct = (_req, _res, _next) => {
-    _res.render('admin/add-product', { pageTitle: 'Add Product', path: "/admin/add-product", formsCSS: true, productCSS: true, activeAddProduct: true });
+    _res.render('admin/edit-product', { pageTitle: 'Add Product', path: "/admin/add-product", editing: false });
 };
 
 exports.postAddProduct = (_req, _res, _next) => {
@@ -9,9 +9,35 @@ exports.postAddProduct = (_req, _res, _next) => {
     const imageUrl = _req.body.imageUrl;
     const price = _req.body.price;
     const description = _req.body.description;
-    const product = new Product(title, imageUrl, description, price);
+    const product = new Product(null, title, price, imageUrl, description);
     product.save();
     _res.redirect('/');
+};
+
+exports.getEditProduct = (_req, _res, _next) => {
+    const editMode = _req.query.edit;
+    if (!editMode) {
+        return _res.redirect('/');
+    }
+    const prodID = _req.params.productId;
+    Product.findById(prodID, product => {
+        if (!product) {
+            return _res.redirect('/');
+        }
+        _res.render('admin/edit-product', { pageTitle: 'Edit Product', path: "/admin/edit-product", editing: editMode, product: product });
+    })
+
+};
+
+exports.postEditProduct = (_req, _res, _next) => {
+    const prodId = _req.body.productId;
+    const updateTitle = _req.body.title;
+    const updatePrice = _req.body.price;
+    const updateImageUrl = _req.body.imageUrl;
+    const updateDesc = _req.body.description;
+    const updateProduct = new Product(prodId, updateTitle, updatePrice, updateImageUrl, updateDesc);
+    updateProduct.save();
+    _res.redirect('/admin/products');
 };
 
 exports.getProduct = (_req, _res, _next) => {
@@ -19,3 +45,9 @@ exports.getProduct = (_req, _res, _next) => {
         _res.render('admin/products', { prods: products, pageTitle: 'Admin Products', path: '/admin/products' });
     });
 };
+
+exports.postDeleteProduct = (_req, _res, _next) => {
+    const prodId = _req.body.productId;
+    Product.deleteById(prodId);
+    _res.redirect('/admin/products');
+}
--- controllers/shop.js
+++ controllers/shop.js
@@ -1,10 +1,18 @@
 const Product = require('../models/product');
+const Cart = require('../models/cart');
 
 exports.getProducts = (_req, _res, _next) => {
     Product.fetchAll((products) => {
         _res.render('shop/product-list', { prods: products, pageTitle: 'Shop', path: '/products' });
     });
 
+};
+
+exports.getProduct = (_req, _res, _next) => {
+    const prodId = _req.params.productId;
+    Product.findById(prodId, product => {
+        _res.render('shop/product-detail', { product: product, pageTitle: product.title, path: '/products' });
+    })
 };
 
 exports.getIndex = (_req, _res, _next) => {
@@ -14,9 +22,38 @@ exports.getIndex = (_req, _res, _next) => {
 }
 
 exports.getCart = (_req, _res, _next) => {
-    _res.render('shop/cart', {
-        path: '/cart',
-        pageTitle: 'Your Cart'
+    Cart.getProducts(cart => {
+        Product.fetchAll(products => {
+            const cartProducts = []
+            for (product of products) {
+                const cartProductData = cart.products.find(prod => prod.id === product.id);
+                if (cartProductData) {
+                    cartProducts.push({ productData: product, qty: cartProductData.qty });
+                }
+            }
+            _res.render('shop/cart', {
+                path: '/cart',
+                pageTitle: 'Your Cart',
+                products: cartProducts
+            });
+
+        });
+    });
+}
+
+exports.postCart = (_req, _res, _next) => {
+    const prodId = _req.body.productId;
+    Product.findById(prodId, (product) => {
+        Cart.addProduct(prodId, product.price);
+    })
+    _res.redirect('/cart');
+}
+
+exports.postCartDeleteProduct = (_req, _res, _next) => {
+    const prodId = _req.body.productId;
+    Product.findById(prodId, product => {
+        Cart.deleteProduct(prodId, product.price);
+        _res.redirect('/cart');
     });
 }
 
